8bdd58e121b2f42dbc758cef2c8244de7d6d032f
Zi Hao <plusbai100@gmail.com>
2025-10-20 18:55:07 +0800

    Nginx dependency automation and deployment done with correct nginx configuration file, makefile and dockerfile.

--- week2_homework_inventory/sql/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE IF NOT EXISTS inventory_db;
-USE inventory_db;
-CREATE TABLE IF NOT EXISTS products (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    quantity INT DEFAULT 0
-);
-
