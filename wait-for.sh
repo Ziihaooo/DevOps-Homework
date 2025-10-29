0818513ae2ab2b7caaefa8e3d6860c3df18b56c8
Zi Hao <plusbai100@gmail.com>
2025-10-29 18:28:28 +0800

    added comment for clarifying how the wait-for.sh work

--- wait-for.sh
+++ wait-for.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 # safer wait-for.sh (works with Docker + tail)
 
+#get the host and port
 HOST="$1"
 PORT="$2"
 shift 2
 
 TIMEOUT=60
+#command to run after the serice is ready like a variable
 CMD=""
 
-# parse args
+# distinguish the timeout and command
 for arg in "$@"; do
   case "$arg" in
     --timeout=*)
@@ -22,17 +24,21 @@ for arg in "$@"; do
   esac
 done
 
-echo "⏳ Waiting for $HOST:$PORT (timeout: ${TIMEOUT}s)..."
+#just a statement for saying start checking
+echo "Waiting for $HOST:$PORT (timeout: ${TIMEOUT}s)..."
 start_ts=$(date +%s)
 
+#keep trying to connect to the host and port within the timeout specified
 while true; do
   nc -z "$HOST" "$PORT" >/dev/null 2>&1
+  #connected
   if [ $? -eq 0 ]; then
     echo "$HOST:$PORT is available!"
     break
   fi
   now_ts=$(date +%s)
   elapsed=$((now_ts - start_ts))
+  #exit with error if cannot
   if [ "$elapsed" -ge "$TIMEOUT" ]; then
     echo "Timeout after ${TIMEOUT}s waiting for $HOST:$PORT"
     exit 1
@@ -40,6 +46,7 @@ while true; do
   sleep 1
 done
 
+#run the command
 if [ -n "$CMD" ]; then
   echo "Executing command: $CMD"
   exec $CMD
