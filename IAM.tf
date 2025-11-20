1cdff648c15fb40ec889978baabbb4450649f423
Zi Hao <plusbai100@gmail.com>
2025-11-20 13:27:07 +0800

    give ec2 access to s3

--- IAM.tf
+++ IAM.tf
@@ -21,6 +21,24 @@ resource "aws_iam_role_policy_attachment" "ssm_core" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
+# EC2 needs S3 Read (fix for 403)
+resource "aws_iam_role_policy" "ssm_ec2_s3_read" {
+  name = "EC2-S3-Read"
+
+  role = aws_iam_role.ssm_role.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = ["s3:GetObject"]
+        Resource = "${aws_s3_bucket.app_artifacts.arn}/*"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_instance_profile" "ssm_profile" {
   name = "EC2-SSM-InstanceProfile"
   role = aws_iam_role.ssm_role.name
@@ -30,6 +48,8 @@ resource "aws_iam_instance_profile" "ssm_profile" {
 data "aws_iam_openid_connect_provider" "bitbucket" {
   url = "https://api.bitbucket.org/2.0/workspaces/distinctioncoding/pipelines-config/identity/oidc"
 }
+
+data "aws_caller_identity" "current" {}
 
 # ========= PIPELINE ROLE =========
 resource "aws_iam_role" "pipeline_oidc_role" {
@@ -56,8 +76,6 @@ resource "aws_iam_role" "pipeline_oidc_role" {
   })
 }
 
-data "aws_caller_identity" "current" {}
-
 # ========= PIPELINE POLICY =========
 resource "aws_iam_policy" "pipeline_s3_access" {
   name = "PipelineOIDCS3Access"
@@ -68,35 +86,35 @@ resource "aws_iam_policy" "pipeline_s3_access" {
 
       # --- S3 deploy & oidc-test folders ---
       {
-        Effect = "Allow",
-        Action = ["s3:PutObject","s3:GetObject"],
+        Effect = "Allow"
+        Action = ["s3:PutObject", "s3:GetObject"]
         Resource = "${aws_s3_bucket.app_artifacts.arn}/oidc-test/*"
       },
       {
-        Effect = "Allow",
-        Action = ["s3:ListBucket"],
-        Resource = aws_s3_bucket.app_artifacts.arn,
+        Effect = "Allow"
+        Action = ["s3:ListBucket"]
+        Resource = aws_s3_bucket.app_artifacts.arn
         Condition = { StringLike = { "s3:prefix" = "oidc-test/*" } }
       },
 
       {
-        Effect = "Allow",
-        Action = ["s3:PutObject","s3:GetObject"],
+        Effect = "Allow"
+        Action = ["s3:PutObject", "s3:GetObject"]
         Resource = "${aws_s3_bucket.app_artifacts.arn}/deploy/*"
       },
       {
-        Effect = "Allow",
-        Action = ["s3:ListBucket"],
-        Resource = aws_s3_bucket.app_artifacts.arn,
+        Effect = "Allow"
+        Action = ["s3:ListBucket"]
+        Resource = aws_s3_bucket.app_artifacts.arn
         Condition = { StringLike = { "s3:prefix" = "deploy/*" } }
       },
 
       # --- SSM SendCommand ---
       {
-        Effect = "Allow",
-        Action = ["ssm:SendCommand"],
+        Effect = "Allow"
+        Action = ["ssm:SendCommand"]
         Resource = [
-          "arn:aws:ssm:ap-southeast-2::document/AWS-RunShellScript",
+          "arn:aws:ssm:ap-southeast-2:${data.aws_caller_identity.current.account_id}:document/AWS-RunShellScript",
           "arn:aws:ec2:ap-southeast-2:${data.aws_caller_identity.current.account_id}:instance/*"
         ]
       }
