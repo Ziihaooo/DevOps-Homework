644566073f424b5320bdd39946b5b51470937f42
Zi Hao <plusbai100@gmail.com>
2025-11-20 14:04:48 +0800

    forgot deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,12 @@ sudo yum install -y docker make awscli
 sudo systemctl enable docker
 sudo systemctl start docker
 
-aws s3 cp s3://$S3_BUCKET/deploy/docker-compose /usr/local/bin/docker-compose --region $AWS_REGION
+aws s3 cp s3://$S3_BUCKET/deploy/docker-compose /usr/local/bin/docker-compose --region ap-southeast-2
 sudo chmod +x /usr/local/bin/docker-compose
 
 sudo mkdir -p /opt/$PROJECT_NAME
-aws s3 cp s3://$S3_BUCKET/deploy/docker-compose.yml /opt/$PROJECT_NAME/docker-compose.yml --region $AWS_REGION
-aws s3 cp s3://$S3_BUCKET/deploy/Makefile /opt/$PROJECT_NAME/Makefile --region $AWS_REGION
+aws s3 cp s3://$S3_BUCKET/deploy/docker-compose.yml /opt/$PROJECT_NAME/docker-compose.yml --region ap-southeast-2
+aws s3 cp s3://$S3_BUCKET/deploy/Makefile /opt/$PROJECT_NAME/Makefile --region ap-southeast-2
 
 echo PROJECT_NAME=$PROJECT_NAME | sudo tee /opt/$PROJECT_NAME/.env
 echo DOCKER_USERZ=$DOCKER_USERZ | sudo tee -a /opt/$PROJECT_NAME/.env
