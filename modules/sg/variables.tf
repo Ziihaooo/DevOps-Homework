31ee053ec7f50721c188cb6af989d74c3fca4a0c
Zi Hao <plusbai100@gmail.com>
2025-11-23 18:23:27 +0800

    sg m module

---
+++ modules/sg/variables.tf
@@ -0,0 +1,30 @@
+variable "name" {
+  type = string
+}
+
+variable "description" {
+  type    = string
+  default = ""
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "ingress" {
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+}
+
+variable "egress" {
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+}
