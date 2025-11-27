c3ef5b9e72fe00ec338406efdfb5ad3689790821
Zi Hao <plusbai100@gmail.com>
2025-11-27 20:50:12 +0800

    wrong path for deploy shell

--- scripts/upload_output.sh
+++ scripts/upload_output.sh
@@ -5,7 +5,7 @@ set -e
 terraform output -json > outputs.json
 
 # Upload the JSON safely to your artifacts bucket
-aws s3 cp outputs.json s3://zihao-dev-artifacts/outputs.json \
+aws s3 cp outputs.json s3://zihao-dev-artifacts/deploy/outputs.json \
     --region ap-southeast-2
 
 echo "✔ outputs.json uploaded to S3!"
