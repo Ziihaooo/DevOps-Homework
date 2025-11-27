8635975d720e02437d54b8b284d829ff6def8c61
Zi Hao <plusbai100@gmail.com>
2025-11-27 21:06:40 +0800

    output.json from correct place

--- scripts/upload_output.sh
+++ scripts/upload_output.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -e
 
+# Move into the Terraform environment directory
+cd ../infra/envs/dev
+
 # Generate JSON file of outputs from Terraform
 terraform output -json > outputs.json
 
-# Upload the JSON safely to your artifacts bucket
-aws s3 cp outputs.json s3://zihao-dev-artifacts/deploy/outputs.json \
-    --region ap-southeast-2
+# Upload outputs.json to S3
+aws s3 cp outputs.json \
+  s3://zihao-dev-artifacts/deploy/outputs.json \
+  --region ap-southeast-2
 
-echo "✔ outputs.json uploaded to S3!"
+echo "✓ outputs.json uploaded to S3!"
